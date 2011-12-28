6cdbfe835e1e9f0ca855383b00b77291fab43c9b
Dan Shipper <dshipper@gmail.com>
2011-12-28 01:26:28 -0500

    idk why this didnt fix it

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Mixmatic::Application.initialize!
 
-config.action_mailer.delivery_method = :smtp
-
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
    :port           => '587',
@@ -15,4 +13,3 @@ ActionMailer::Base.smtp_settings = {
    :domain         => 'heroku.com'
 }
 
-config.action_mailer.raise_delivery_errors = true
