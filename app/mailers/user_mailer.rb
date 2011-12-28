5596a3b5ea7582a1ddde2f950c6bb31f1c2efa4b
Dan Shipper <dshipper@gmail.com>
2011-12-28 01:39:05 -0500

    a few mailer tweaks

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ActionMailer::Base
-  default from: "dan@danshipper.com"
+  default from: "no-reply@mixmatic.co"
   
   def you_got_mix (sender_name, recipient_name, link, email)
      @name = sender_name
      @person = recipient_name    
      @link = link     
-     subject = ""
+     subject = @person + " created you a mixtape with Mixmatic"
      mail(:to => email, 
           :subject => subject)  
    end
