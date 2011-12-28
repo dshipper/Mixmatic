579321eae537dfe657b76a65bdfddb3fcbc0965a
Dan Shipper <dshipper@gmail.com>
2011-12-28 01:24:43 -0500

    fixed a bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,8 @@ Mixmatic::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.raise_delivery_errors = true
 end
