e55a94e3a342037a9503d7d114c3d9a805eecf23
Dan Shipper <dshipper@gmail.com>
2011-12-28 02:04:24 -0500

    added mix name to emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,11 @@
 class UserMailer < ActionMailer::Base
   default from: "no-reply@mixmatic.co"
   
-  def you_got_mix (sender_name, recipient_name, link, email)
+  def you_got_mix (sender_name, recipient_name, link, email, mix_name)
      @name = sender_name
      @person = recipient_name    
-     @link = link     
+     @link = link    
+     @mix_name = mix_name 
      subject = @name + " created a mixtape for you with Mixmatic"
      mail(:to => email, 
           :subject => subject)  
