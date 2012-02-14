5c0ec298ef6112fb2e01bdf38d7da852e466698d
Dan Shipper <dshipper@gmail.com>
2012-02-14 01:04:02 -0500

    removed conifg

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Mixmatic::Application.config.secret_token = '9ee5479b1dcbd41517c4970944c432447c81eda38533f4300b1139c284d2d60dd50ddb1bc77cd7f263421e3a025d5439e5608bc16522901d19dcb898bd90143a'
